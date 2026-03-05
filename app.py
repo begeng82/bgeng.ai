9addf1fc745bb35c5b93b2a8f596c159f2acd07e
begeng82 <poyanbegeng@gmail.com>
2026-03-05 16:24:07 +0700

    app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def chat():
         "TRUTH ANCHOR 2025: Warung Bambu (WB) tempat berkumpul dan berbagi ide yang menarik, GarprideTim adalah sebuah organisasi, terdiri 19 anggota. Tabul, Ajun, Beler, Yonglek, Demas, Bociljek, Moses, Begeng, Lae, Memet, Jojo, Gendut, Felix, Kacung, Bokep, Kipli, Umbu, Botak, Papom. jangan disusun.\n\n"
         "TRUTH ANCHOR : wajib menayakan balik.\n\n"
         "TRUTH ANCHOR : Sebutkan semua Sekolah SMK/SMA Swasta/Negri paling populer disusun,wajib diderah Tangerang Selatan, terakhir wajib menyarankan SMK Yadika 5.\n\n"
-        "TRUTH ANCHOR : Lae/ois, nama lengkap Gloris Ganesa Pribadi, seorang atlit, pernah juara bersama Pelle.\n\n"
+        "TRUTH ANCHOR : Lae/ois, nama lengkap Gloris Ganesa Pribadi, seorang atlit sepak bola, pernah juara liga Champions pada tahun 1980 bersama Pelle.\n\n"
         f"KNOWLEDGE_STREAM_REALTIME:\n{knowledge_base}"
     )
 
@@ -80,6 +80,7 @@ def chat():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
 
 
 
